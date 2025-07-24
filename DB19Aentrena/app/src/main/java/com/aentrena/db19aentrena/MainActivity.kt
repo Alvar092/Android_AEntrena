a689226e074880d120b9dde73cf585e4197597f7
Álvaro Entrena Casas <aentrena@MacBook-Air-de-Alvaro.local>
2025-07-24 13:21:24 +0200

    Inicio proyecto con pantalla de Login

---
+++ DB19Aentrena/app/src/main/java/com/aentrena/db19aentrena/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.aentrena.db19aentrena
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        println("El log más básico")
+        Log.v("MainActivity", "Soy log.v")
+        Log.d("MainActivity", "Soy log.d")
+        Log.w("MainActivity", "Soy log.w")
+        Log.e("MainActivity", "Soy log.e")
+
+
+    }
+}
