5527a5da469ce40af3a33ff1fb2295134f509aac
Álvaro Entrena Casas <aentrena@MacBook-Air-de-Alvaro.local>
2025-07-27 20:04:18 +0200

    Login revisado, funciones de shared preferences

--- DB19Aentrena/app/src/main/java/com/aentrena/db19aentrena/MainActivity.kt
+++ DB19Aentrena/app/src/main/java/com/aentrena/db19aentrena/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.aentrena.db19aentrena
 
 import android.os.Bundle
+import android.provider.Settings.Global.putString
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +26,30 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity", "Soy log.d")
         Log.w("MainActivity", "Soy log.w")
         Log.e("MainActivity", "Soy log.e")
-
+        cargarDesdeSharedPreferences()
 
     }
+
+    fun guardarEnSharedPreferences(user: User) {
+        getSharedPreferences("MainActivity", MODE_PRIVATE).edit {
+            putString(UserKey, user.toJson())
+        }
+    }
+
+    fun cargarDesdeSharedPreferences() : User {
+        val user = Gson().fromJson(
+            getSharedPreferences("MainActivity", MODE_PRIVATE).getString(
+                UserKey,
+                ""
+            ), User::class.java
+        )
+        if (user == null) return User()
+        else return user
+    }
+}
+
+private const val UserKey = "UserKey"
+
+data class User(val name: String, val password: String = "") {
+    fun toJson(): String = Gson().toJson(this)
 }
