d3ad5c9657bf24b01d1e4f0fed16234dc1bf8239
Álvaro Entrena Casas <aentrena@MacBook-Air-de-Alvaro.local>
2025-07-28 12:07:28 +0200

    Establezco el binding para la UI

--- DB19Aentrena/app/src/main/java/com/aentrena/db19aentrena/MainActivity.kt
+++ DB19Aentrena/app/src/main/java/com/aentrena/db19aentrena/MainActivity.kt
@@ -8,14 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aentrena.db19aentrena.databinding.ActivityLoginBinding
 import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
